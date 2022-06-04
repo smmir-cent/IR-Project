cd0e8679b2f40e3e6e5d4317de77ba28421be8bf
smmir-cent <76453689+smmir-cent@users.noreply.github.com>
2022-06-04 20:03:50 +0430

    01/03/14 - 20:03 => complete part 2

--- src/second_phase/main.py
+++ src/second_phase/main.py
@@ -8,7 +8,6 @@ import pickle
 from hazm import *
 from collections import Counter
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 import numpy as np
 
 STOPWORDS = True
@@ -26,6 +25,11 @@ token_size_heaps_points = []
 dict_size_heaps_points = []
 pos_index = {}
 tfidf_weights = {}
+query_tfidf_weights = {}
+result_weights = {}
+documents_length = {}
+champion_lists = {}
+
 heaps_points = [500, 1000, 1500, 2000, json_object_size]
 
 def loadIndex():
@@ -100,82 +104,22 @@ def merging(pl1,pl2):
     return res
 
 
-def softMerging(pl1,pl2):
-    pl1.extend(pl2)
-    return pl1
-
-def checkSeq(l1,l2):
-    counter_1, counter_2 = 0, 0
-    size_1, size_2 = len(l1), len(l2)
-    state = False
-    while counter_1 < size_1 and counter_2 < size_2:
-        if l1[counter_1] + 1 == l2[counter_2]:
-            state = True
-            counter_1 += 1
-            counter_2 += 1
-        elif l1[counter_1] < l2[counter_2]:
-            counter_1 += 1
-        else:
-            counter_2 += 1
-    return state
 
 def ranking(all_terms,final_res):
-    ranks = {}
+    print(final_res)
     display_pos = {}
     for docid in final_res:
-        ranks[docid] = 0
         display_pos[docid] = []
     for term in all_terms:
         for docid in final_res:
             if docid in pos_index[term][1]:
-                ranks[docid] += len(pos_index[term][1][docid])
                 display_pos[docid].append(int(sum(pos_index[term][1][docid])/len(pos_index[term][1][docid])))
-    return sorted(ranks, key=ranks.get, reverse=True)[:5],display_pos
+    return sorted(final_res, key=final_res.get, reverse=True)[:5],display_pos
 
-
-def execPhrases(phrases):
-    global all_terms
-    phrase_result = []
-    first_phrase = True
-    for phrase in phrases:
-        norm = normalizer.normalize(phrase)
-        tokens = word_tokenize(norm)
-        docid_candidates = []
-        for item in deepcopy(tokens):
-            if item not in stopwords:
-                stemming = stemmer.stem(item)
-                ind = tokens.index(item)
-                tokens = tokens[:ind]+[stemming]+tokens[ind+1:]
-            else:
-                tokens.remove(item)
-        try:
-            docid_candidates = list(pos_index[tokens[0]][1].keys())
-            i = 1
-            while  i < len(tokens):
-                docid_candidates = merging(docid_candidates,list(pos_index[tokens[i]][1].keys()))
-                i += 1
-        except:
-            docid_candidates = []
-        all_terms.extend(tokens)
-        counter = 0
-        res = []
-        while counter < len(tokens) - 1:
-            for docid in docid_candidates:
-                l1 = pos_index[tokens[counter]][1][docid]
-                l2 = pos_index[tokens[counter+1]][1][docid]
-                state = checkSeq(l1,l2)
-                if state:
-                    res.append(docid)
-            counter += 1
-        if first_phrase:
-            first_phrase = False
-            phrase_result = res
-        phrase_result = sorted(list(set(phrase_result) & set(res))) 
-    return phrase_result
 
 
 def queryProcessing(query):
-    global all_terms
+    global all_terms,json_object_size,query_tfidf_weights,tfidf_weights,result_weights
     ######### preprocessing #########
     phrases = re.findall('"([^"]*)"', query)
     terms = re.sub('"([^"]*)"', '', query)
@@ -207,36 +151,27 @@ def queryProcessing(query):
 
     ########### calculating tfidf_weights ###########
 
-    query_tfidf_weights = {}
     for term in counters.keys():
         n_t = len(pos_index[term][1])
         query_tfidf_weights[term] = (1 + math.log10(counters[term])) * math.log10(json_object_size/n_t)
     ########### /calculating tfidf_weights ###########
 
-    ### and_terms ###
-    try:
-        tokens = list(dict.fromkeys(tokens))
-        final_res = list(pos_index[tokens[0]][1].keys())
-        i = 1
-        while  i < len(tokens):
-            final_res = softMerging(final_res,list(pos_index[tokens[i]][1].keys()))
-            i += 1
-    except:
-        final_res = []
-    # print("######### and_terms #########")  
-    z = Counter(final_res)
-    # print(z)
-    final_res = []
-    for key in z:
-        if z[key] > len(tokens) - 2:
-            final_res.append(key)
-    final_res = sorted(final_res)
-    ### /and_terms ###
+    for term in counters.keys():
+        for docid in tfidf_weights[term]:
+            if docid in result_weights:
+                result_weights[docid] += query_tfidf_weights[term] * tfidf_weights[term][docid]
+            else:
+                result_weights[docid] = query_tfidf_weights[term] * tfidf_weights[term][docid]
+    for docid in result_weights.keys():
+         result_weights[docid] /= math.sqrt(documents_length[docid])
+    
+
 
 
     ######### /preprocessing #########
 
-    ranked_doc,display_pos = ranking(all_terms=all_terms,final_res=final_res)
+    ranked_doc,display_pos = ranking(all_terms=counters.keys(),final_res=result_weights)
+    print(ranked_doc)
     f = open("result.txt", "w", encoding='utf-8')
     for i,doc in enumerate(ranked_doc):
         display_center = int(sum(display_pos[doc])/len(display_pos[doc]))
@@ -259,7 +194,7 @@ def queryProcessing(query):
             if thr > display_center:
                 break
             counter += 1
-        f.write(str(i+1)+") \n\n")
+        f.write(str(i+1)+") score "+str(result_weights[doc])+" \n\n")
         f.write("title:  "+title+"\n\n")
         f.write("url:  "+url+"\n\n")
         f.write(content[index-100:index+100]+"\n\n")
@@ -279,23 +214,20 @@ def loadStopwords():
 
 
 def tfidfCal():
-    global pos_index,tfidf_weights
+    global pos_index,tfidf_weights,documents_length
     print("before cal len(pos_index)",len(pos_index))
-    # for _ in range(0,1000):
-    #     print(temp)
-    #     print(temp[0])
-    #     print(temp[1])
     for term in pos_index.keys():
         temp = pos_index[term]
-        
-        #### temp[1][1].keys: term,doc
         tfidf_weights[term] = {}
         n_t = len(temp[1])
         for keys_docs in temp[1].keys():
             doc_term = temp[1][keys_docs]
-            doc_id = doc_term[0] 
-            f_td =  len(doc_term[1])
-            tfidf_weights[term][doc_id] = (1 + math.log10(f_td))*math.log10(json_object_size/n_t)
+            f_td =  len(doc_term)
+            tfidf_weights[term][keys_docs] = (1 + math.log10(f_td))*math.log10(json_object_size/n_t)
+            if keys_docs in documents_length:
+                documents_length[keys_docs] += tfidf_weights[term][keys_docs]**2
+            else:
+                documents_length[keys_docs] = tfidf_weights[term][keys_docs]**2
     print("after cal len(pos_index)",len(pos_index))
 
 
